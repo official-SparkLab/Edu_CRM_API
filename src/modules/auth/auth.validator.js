1065b3e3ab30f6efab0f59af97ff5b9904029b04
rohan-kadukar <112863986+rohan-kadukar@users.noreply.github.com>
2025-08-19 13:20:23 +0530

    Cache work on branch route, main-branch route created and super-admin registration route changed (branch_id required)

--- src/modules/auth/auth.validator.js
+++ src/modules/auth/auth.validator.js
@@ -3,6 +3,15 @@
 
 const { body } = require("express-validator");
 
+const validateMainBranch = [
+  body('branch_name').notEmpty().withMessage('Branch name is required'),
+  body('email').isEmail().withMessage('Valid email is required'),
+  body('phone').notEmpty().withMessage('Phone is required'),
+  body('address').notEmpty().withMessage('Address is required'),
+  body('pincode').notEmpty().isNumeric().withMessage('Pincode is required and must be numeric'),
+  body('established_date').notEmpty().withMessage('Established date is required'),
+];
+
 const validateSuperAdmin = [
   body("user_name").notEmpty().withMessage("User name is required"),
   body("contact")
@@ -13,6 +22,7 @@ const validateSuperAdmin = [
   body("password")
     .isLength({ min: 6 })
     .withMessage("Password must be at least 6 characters"),
+  body('branch_id').notEmpty().isNumeric().withMessage('Branch id is required and must be numeric'),
 ];
 
 const validateLogin = [
@@ -20,4 +30,4 @@ const validateLogin = [
   body("password").notEmpty().withMessage("Password is required"),
 ];
 
-module.exports = { validateSuperAdmin, validateLogin };
+module.exports = { validateMainBranch, validateSuperAdmin, validateLogin };
